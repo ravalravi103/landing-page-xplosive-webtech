cfc188436b9dcfc1551c4e26be0cf97f171cb187
ravi.raval <ravi.raval@neosoftmail.com>
2026-04-26 20:12:34 +0530

    route addded

--- src/App.tsx
+++ src/App.tsx
@@ -15,14 +15,15 @@ const App = () => (
       <Toaster />
       <Sonner />
       {/* <BrowserRouter> */}
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </HashRouter>
+      {/* <HashRouter> */}
+      {/* <Routes> */}
+      {/* <Route path="/" element={<Index />} /> */}
+      {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+      {/* <Route path="*" element={<NotFound />} /> */}
+      {/* </Routes> */}
+      {/* </HashRouter> */}
       {/* </BrowserRouter> */}
+      <Index />
     </TooltipProvider>
   </QueryClientProvider >
 );
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/landing-page-xplosive-webtech/",
+  base: "/landing-page-xplosive-webtech",
   server: {
     host: "::",
     port: 3000,
